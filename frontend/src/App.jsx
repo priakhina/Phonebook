df5f44c8648d2a16d7d9f95b27cbe5d6dfc99ee8
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-11-09 20:11:49 -0700

    Implement the addition of names to the phonebook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,44 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [contacts, setContacts] = useState([{ name: 'John Doe' }]);
+  const [newContactName, setNewContactName] = useState('');
+
+  const addContact = (e) => {
+    e.preventDefault();
+
+    const newContact = {
+      name: newContactName,
+    };
+
+    setContacts([...contacts, newContact]);
+    setNewContactName('');
+  };
 
   return (
-    <>
+    <div>
+      <h1>Phonebook</h1>
+      <form onSubmit={addContact}>
+        <div>
+          Name:{' '}
+          <input
+            value={newContactName}
+            onChange={({ target }) => setNewContactName(target.value)}
+          />
+        </div>
+        <div>
+          <button type='submit'>Add a new contact</button>
+        </div>
+      </form>
+      <h2>All contacts</h2>
       <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+        {contacts.map((contact) => (
+          <p key={contact.name}>{contact.name}</p>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
